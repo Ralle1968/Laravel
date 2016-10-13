089a27b6a0fc17e3b9c50de89833e75e14d9c324
Ralle1968 <graef.ralf@gmail.com>
2016-10-13 19:23:42 +0200

    Set up 1 draft

---
+++ resources/assets/js/components/Simple.vue
@@ -0,0 +1,50 @@
+<template>      
+<div class="container"> 
+<div class="row">
+<div class="col-sm-6">    
+<h1>Ralfs Todo-Liste</h1>    
+    <div id="todo-list-example">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="(todo, index) in todos" v-bind:title="todo" >
+                {{ todo }}
+                <button v-on:click="removeTodo(index)" class="btn btn-warning btn-xs pull-right" > Löschen </button>          
+            </li>
+        </ul>
+        <br>
+        <input v-model="newTodoText" v-on:keyup.enter="addNewTodo" class="form-control" placeholder="Hier neuen ToDo eingeben">
+    </div>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+
+    data(){
+        return {
+            newTodoText: '',
+            todos: [
+              'ToDo-App erstellen',
+              'Termin eintragen',
+              'Fenster putzen'
+            ]
+        };
+    },
+
+    methods: {
+        addNewTodo: function () {
+          this.todos.push(this.newTodoText)
+          this.newTodoText = ''
+        },
+        removeTodo: function(index) {
+            this.todos.splice(index, 1)
+        }
+    },
+
+        mounted() {
+            console.log('Component <simple> ready.');
+        }
+}
+</script>
